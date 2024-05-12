d9c2d130043bf84c285e264baea47a390348d014
Jimmy B. <chips_xp@yahoo.com>
2024-05-12 09:54:09 -0400

    wp blocks to graph

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    //maps wp block to graphql
+    ".node_modules/@wp-block-tools/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
